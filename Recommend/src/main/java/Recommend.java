90c198838a4a20cd1b05b96aa37bd293b6a2a6b1
brandonccm1996 <brandonccm@hotmail.com>
2020-09-11 01:52:16 +0800

    Complete part 1 of recommend
    To group scores by users

--- Recommend/src/main/java/Recommend.java
+++ Recommend/src/main/java/Recommend.java
@@ -12,47 +12,51 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Recommend {
-    public static class Mapper1 extends Mapper<Object, Text, Text, IntWritable> {
 
-        private final static IntWritable one = new IntWritable(1);
-        private Text word = new Text();
+    public static class Mapper1 extends Mapper<Object, Text, IntWritable, Text> {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-            StringTokenizer itr = new StringTokenizer(value.toString());
-            while (itr.hasMoreTokens()) {
-                word.set(itr.nextToken());
-                context.write(word, one);
-            }
+            String[] userItemScore = value.toString().split(",");
+            int user = Integer.parseInt(userItemScore[0]);
+            String item = userItemScore[1];
+            String score = userItemScore[2];
+
+            context.write(new IntWritable(user), new Text(item + "," + score));
         }
     }
 
-    public static class Reducer1 extends Reducer<Text,IntWritable,Text,IntWritable> {
-        private IntWritable result = new IntWritable();
+    public static class Reducer1 extends Reducer<IntWritable, Text, IntWritable, Text> {
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+        public void reduce(IntWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            System.out.println(key);
+            StringBuilder userScoresList = new StringBuilder();
+            for (Text value : values) {
+                userScoresList.append(value.toString() + "|");
             }
-            result.set(sum);
-            context.write(key, result);
+
+            // to remove the trailing |
+            userScoresList.setLength(userScoresList.length()-1);
+
+            context.write(key,new Text(userScoresList.toString()));
         }
     }
 
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
-        Job job = Job.getInstance(conf, "recommend");
+        Job job1 = Job.getInstance(conf, "recommend part 1");
 
-        job.setJarByClass(Recommend.class);
-        job.setMapperClass(Mapper1.class);
-        job.setCombinerClass(Reducer1.class);
-        job.setReducerClass(Reducer1.class);
+        job1.setJarByClass(Recommend.class);
+        job1.setMapperClass(Mapper1.class);
+        job1.setCombinerClass(Reducer1.class);
+        job1.setReducerClass(Reducer1.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        job1.setOutputKeyClass(IntWritable.class);
+        job1.setOutputValueClass(Text.class);
+        FileInputFormat.addInputPath(job1, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job1, new Path("temp_output1"));
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        job1.waitForCompletion(true);
+
+//        System.exit(job1.waitForCompletion(true) ? 0 : 1);
     }
 }
