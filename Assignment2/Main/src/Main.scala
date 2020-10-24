e4b96bd70c85d0b8818711583882566fff9f821d
brandonccm1996 <brandonccm@hotmail.com>
2020-10-24 23:11:21 +0800

    Add stopword removal, word2vec, and k means clustering

--- Assignment2/Main/src/Main.scala
+++ Assignment2/Main/src/Main.scala
@@ -14,6 +14,43 @@ object Main {
 
     // write your code here
 
+    // Remove stopwords
+    val stopWordLines = spark.read.text(args(0))
+    val stopWordLinesArray = stopWordLines.collect.map(row=>row.getString(0))
+
+    val tweetsDataFrame = spark.read.format("csv").load(args(1)).select("_c5")
+    tweetsDataFrame.show(2, false)
+
+    val tokenizer = new Tokenizer().setInputCol("_c5").setOutputCol("tweetsWordsRaw")
+    val tokenized: DataFrame = tokenizer.transform(tweetsDataFrame)
+
+    val remover = new StopWordsRemover().setInputCol("tweetsWordsRaw").setOutputCol("tweetsWordsFiltered").setStopWords(stopWordLinesArray)
+    val filtered: DataFrame = remover.transform(tokenized).select("tweetsWordsFiltered")
+    filtered.show(2, false)
+
+    // Convert to word2vec format
+    val word2vec = new Word2Vec().setInputCol("tweetsWordsFiltered").setOutputCol("word2vec_result")
+    val word2vec_model = word2vec.fit(filtered)
+    val word2vecDataframe = word2vec_model.transform(filtered).select("word2vec_result")
+    word2vecDataframe.show(2, false)
+    word2vecDataframe.cache()
+
+    // Trains a k-means model
+    val kmeans = new KMeans().setK(400).setFeaturesCol("word2vec_result").setPredictionCol("predicted_cluster")
+    val kmeans_model = kmeans.fit(word2vecDataframe)
+    val dataframeClustered = kmeans_model.transform(word2vecDataframe)
+    dataframeClustered.show(2, false)
+
+    // Evaluate clustering by computing Silhouette score
+    val evaluator = new ClusteringEvaluator().setFeaturesCol("word2vec_result").setPredictionCol("predicted_cluster")
+
+    val silhouette = evaluator.evaluate(dataframeClustered)
+    println(s"Silhouette with squared euclidean distance = $silhouette")
+
+    // Shows the result
+    println("Cluster Centers: ")
+    kmeans_model.clusterCenters.foreach(println)
+
 
     spark.stop()
   }
